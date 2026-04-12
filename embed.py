cdd8735385648a55ea84a824095cc9c4bbf54a7b
jayant <iamvermajayant@gmail.com>
2026-04-12 22:01:25 +0530

    added the embed hugging face token

--- embed.py
+++ embed.py
@@ -1,7 +1,17 @@
+# from sentence_transformers import SentenceTransformer
+
+# # Load model once (ok here)
+# model = SentenceTransformer("all-MiniLM-L6-v2")
+
+# def get_embedding(texts):
+#     return model.encode(texts)
+
+
+
 from sentence_transformers import SentenceTransformer
+import os
 
-# Load model once (ok here)
-model = SentenceTransformer("all-MiniLM-L6-v2")
-
-def get_embedding(texts):
-    return model.encode(texts)
+model = SentenceTransformer(
+    "all-MiniLM-L6-v2",
+    use_auth_token=os.getenv("HF_TOKEN")
+)
