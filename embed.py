584fc11f04d965e7522bc032ce03a1f9e5e81873
jayant <iamvermajayant@gmail.com>
2026-04-16 20:29:26 +0530

    making changes in the embed.py file

--- embed.py
+++ embed.py
@@ -1,13 +1,3 @@
-# from sentence_transformers import SentenceTransformer
-
-# # Load model once (ok here)
-# model = SentenceTransformer("all-MiniLM-L6-v2")
-
-# def get_embedding(texts):
-#     return model.encode(texts)
-
-
-
 from sentence_transformers import SentenceTransformer
 import os
 
@@ -15,3 +5,6 @@ model = SentenceTransformer(
     "all-MiniLM-L6-v2",
     use_auth_token=os.getenv("HF_TOKEN")
 )
+
+def get_embedding(texts):
+    return model.encode(texts)
